b7ba30acac44f8c09b1f0bc72fa049b3de5bfb56
Joerg Reichert <de.abg.reichert.joerg@googlemail.com>
2016-06-23 00:42:58 +0200

    fixed file

--- karma.config.js
+++ karma.config.js
@@ -1,5 +1,9 @@
 module.exports = function(config) {
   config.set({
+    frameworks: ['mocha'],
     browsers: ['Firefox', 'FirefoxDeveloper', 'FirefoxAurora', 'FirefoxNightly'],
+    files: [
+      '*.js'
+    ]
   });
 };
